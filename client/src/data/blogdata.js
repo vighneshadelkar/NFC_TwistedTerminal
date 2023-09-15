788417c5fbd118cb13352b9df73c713e71eadd59
vighneshadelkar <vighnesh.adelkar21@gmail.com>
2023-09-15 23:11:30 +0530

    added blogdata

---
+++ client/src/data/blogdata.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        "id": 1,
+        "image": "Person2",
+        "name": "Priya",
+        "quote": "Ayurveda, the ancient Indian system of medicine, offers a profound approach to holistic well-being. Rooted in a deep understanding ofnature's rhythms and the individual's constitution, Ayurveda seeks tobalance mind, body, and spirit. Through personalized dietaryrecommendations, herbal remedies, and lifestyle practices, it empowersindividuals to cultivate health and vitality."
+    }, {
+        "id": 2,
+        "image": "Person1",
+        "name": "Vighnesh",
+        "quote": "Ayurveda, an ancient Indian system, harmonizes body, mind, and spirit. Tailored nutrition, herbal remedies, and mindful living promote vitality. This timeless wisdom offers a balanced, holistic approach to health."
+    }, {
+        "id": 3,
+        "image": "Person3",
+        "name": "Abuzaid",
+        "quote": "Ayurveda, an ancient Indian system, harmonizes body, mind, and spirit. Tailored nutrition, herbal remedies, and mindful living promote vitality. This timeless wisdom offers a balanced, holistic approach to health."
+    }
+]
+export default data;
