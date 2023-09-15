f28cdb5833803588029f8078b1c2d9a0044d935a
vighneshadelkar <vighnesh.adelkar21@gmail.com>
2023-09-15 17:03:17 +0530

    changed svg details

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -89,7 +89,7 @@ export default function Login() {
                     d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
                     stroke-width="2"
                     stroke-linejoin="round"
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                   ></path>
                 </svg>
               </span>
@@ -117,13 +117,13 @@ export default function Login() {
                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                     stroke-width="2"
                     stroke-linejoin="round"
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                   ></path>
                   <path
                     d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
                     stroke-width="2"
                     stroke-linejoin="round"
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                   ></path>
                 </svg>
               </span>
